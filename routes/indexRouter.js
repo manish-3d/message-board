5d6f288359c8747c597e78640e361999b1a647d1
manish-3d <manishkumar.intc@gmail.com>
2026-05-18 22:38:55 +0530

    building router

---
+++ routes/indexRouter.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+    {
+        text : "hi there", 
+        user : "amando",
+        added : new Date()
+    },
+    {
+        text : "hello there", 
+        user : "charles",
+        added : new Date()
+    }
+];
+router.get("/", (req,res) => {
+    res.render("index",{
+        title : "Mini messageboard",
+        messages : messages
+    });
+});
+router.get("/new",(req,res) => {
+    res.render("form");
+});
+router.post("/new", (req , res) => {
+    const messageText = req.body.messageText;
+    const messageUser = req.body.messageUser;
+
+    messages.push({
+        text : messageText,
+        user : messageUser,
+        added : new Date()
+    });
+res.redirect("/");
+});
+module.exports = router;
